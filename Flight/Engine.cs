ceb43b7b0f976acf81846d4c98d7f4116c206a3e
Ammarru <62930502+Ammarru@users.noreply.github.com>
2021-03-09 13:12:05 +0700

    finished

---
+++ Flight/Engine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+	class Engine
+	{
+        const double dt = 0.1;
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        public double a;
+        public double v0;
+        public double y0;
+        public double S;
+        public double m;
+        double k;
+
+        public double t;
+        public double x;
+        public double y;
+        public double vx;
+        public double vy;
+
+      
+
+       
+		
+		public void start(Form1 form)
+		{
+           form.start();
+            k = 0.5 * C * S * rho / m;
+
+            vx = v0 * Math.Cos(a * Math.PI / 180);
+            vy = v0 * Math.Sin(a * Math.PI / 180);
+
+            t = 0;
+            x = 0;
+            y = y0;
+        }
+
+        public void tick()
+		{
+            t += dt;
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+        }
+    }
+}
---
+++ Flight/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        private static Form1 form;
+        public Form1()
+        {
+            InitializeComponent();
+            form = this;
+        }
+
+       
+        Engine Engine = new Engine();
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Engine.start(this);
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(Engine.x, Engine.y);
+
+            timer1.Start();
+        }
+        public void start()
+		{
+            Engine.a = (double)edAngle.Value;
+            Engine.v0 = (double)edSpeed.Value;
+            Engine.y0 = (double)edHeight.Value;
+            Engine.m = (double)edWeight.Value;
+            Engine.S = (double)edSquare.Value; ;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Engine.tick();
+
+            chart1.Series[0].Points.AddXY(Engine.x, Engine.y);
+            if (Engine.y <= 0) timer1.Stop();
+        }
+    }
+}
